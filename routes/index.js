5314666ec9e56d7ceb28a91e3b90b6853bf660bd
sungkuk5420 <sungkuk5420@gmail.com>
2018-07-03 19:05:51 +0900

    첫커밋

---
+++ routes/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const async = require('async');
+const Upload = require('../service/UploadService');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/upload', (req, res) => {
+  const tasks = [
+    (callback) => {
+      console.log('start formidable');
+      Upload.formidable(req, (err, files, fields) => {
+        callback(err, files, fields);
+      });
+    },
+    (files, fields, callback) => {
+      console.log('start optimize');
+      Upload.optimize(files, (err) => {
+        callback(err, files, fields);
+      });
+    },
+    (files, fields, callback) => {
+      console.log('start s3');
+      Upload.s3(files, 'test/', (err, result) => {
+        console.log('err : ' + err);
+        console.log('result : ' + result);
+        callback(err, result)
+      });
+    }
+  ];
+  async.waterfall(tasks, (err, result) => {
+    if (!err) {
+      res.json({success: true, msg: '업로드 성공'})
+    } else {
+      res.json({success: false, msg: '업로드 실패'})
+    }
+  });
+});
+
+module.exports = router;
---
+++ service/UploadService.js
@@ -0,0 +1,95 @@
+const formidable = require('formidable');
+const async = require('async');
+const AWS = require('aws-sdk');
+const imagemin = require('imagemin');
+const imageminPngquant = require('imagemin-pngquant');
+AWS.config.update({
+    accessKeyId: "",
+    secretAccessKey: "",
+    "region": "ap-northeast-2"  
+});
+const S3 = new S3Instance();
+const ROOT_PATH = process.cwd();
+const Upload = {};
+
+function S3Instance() {
+  'use strict';
+  let instance;
+  S3Instance = function () {
+    return instance;
+  };
+  instance = new AWS.S3();
+  return instance
+}
+
+/*S3 버킷 설정*/
+let params = {
+  Bucket: '',
+  Key: '',
+  ACL: 'public-read',
+  Body: null
+};
+
+Upload.formidable = (req, callback) => {
+  let _fields;
+  
+  function FormidableInstance() {
+    'use strict';
+    let instance;
+    FormidableInstance = function () {
+      return instance;
+    };
+    
+    instance = new formidable.IncomingForm({
+      encoding: 'utf-8',
+      multiples: true,
+      keepExtensions: false, //확장자 제거
+      uploadDir: `${ROOT_PATH}/service`
+    });
+    
+    return instance
+  }
+  
+  const form = new FormidableInstance();
+  
+  form.parse(req, function (err, fields) {
+    _fields = fields;
+  });
+  
+  form.on('error', function (err) {
+    callback(err, null, null);
+  });
+  
+  form.on('end', function () {
+    callback(null, this.openedFiles, _fields);
+  });
+};
+
+Upload.optimize = (files, callback) => {
+  async.each(files, (file, cb) => {
+    imagemin([file.path], `${ROOT_PATH}/service/`, {
+      plugins: [
+        imageminPngquant({quality: '0-80', verbose: false, floyd: 1})
+      ]
+    }).then(() => {
+      cb();
+    })
+  }, (err) => {
+    callback(err)
+  });
+};
+
+Upload.s3 = (files, key, callback) => {
+  async.each(files, (file, cb) => {
+    params.Key = key + file.name;
+    params.Body = require('fs').createReadStream(file.path);
+  
+    S3.upload(params, (err, result) => {
+      cb(err, result);
+    });
+  }, (err, result) => {
+    callback(err, result);
+  });
+};
+
+module.exports = Upload;
